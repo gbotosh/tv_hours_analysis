0a1efb67c6b3037cb3e574ad8a4e882f928c5907
Gbotoso Olaoluwa <laolugbotoso@yahoo.com>
2024-04-16 16:00:45 +0200

    make names longer

--- tv_hours_analysis.R
+++ tv_hours_analysis.R
@@ -1,9 +1,9 @@
 library(tidyverse)
 library(here)
 
-tv_hours_table <- gss_cat %>% 
+tv_hours_tabulation <- gss_cat %>% 
                   filter(age < 30) %>% 
                   group_by(marital) %>% 
                   summarise(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 
-write_csv(tv_hours_table, here("tv_hours_by_marital"))
+write_csv(tv_hours_tabulation, here("tv_hours_by_marital"))
